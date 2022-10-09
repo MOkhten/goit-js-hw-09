8704dcefc4993fa663764676892a2393330ded8d
MOkhten <m.okhten@gmail.com>
2022-10-09 21:45:50 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,3 +2,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const currentColor = getRandomHexColor();
+    body.style.backgroundColor = currentColor;
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
+
